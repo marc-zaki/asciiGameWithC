8d28caeb66f78aa93c4e2503ddf24afac1d239c8
sherifmark759 <sherifmark759@gmail.com>
2024-12-11 08:57:58 +0200

    added movement and a platform

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -11,6 +11,13 @@ void Empty(char game[][120]) {
 		}
 	}
 }
+void drawPlatform(char platform[][120], int platformR, int platformC) {
+	for (int i = 0; i < 100; i++)
+	{
+		platform[platformR][platformC + i] = '=';
+
+	}
+}
 void drawGame(char game[][120]) {
 	system("cls");
 	for (int r = 0; r < 30; r++)
@@ -33,16 +40,46 @@ void drawHero(char game[][120], int& rhero, int& chero) {
 	game[rhero - 1][chero + 5] = '\\';
 	game[rhero - 1][chero + 6] = '_';
 }
+void moveHero(int &rhero, int &chero, char move, char game[][120]) 
+{
+	if (move == 'w')
+	{
+		rhero-=2;
+	}
+	if (move == 'a')
+	{
+		chero--;
+	}
+	if (move == 's' && game[rhero][chero] != '=')
+	{
+		rhero++;
+	}
+	if (move == 'd')
+	{
+		chero++;
+	}
+}
+void gravity(char game[][120], int& rHero, int&cHero) {
+	if (game[rHero][cHero] == ' ')
+	{
+		rHero++;
+	}
+}
 void main() {
 	char game[30][120];
-	int rhero = 24, chero = 5;
+	int rhero = 24, chero = 5, platformR = 27, platformC = 2;
+	char move=NULL;
 	while(1) {
 		while (!_kbhit()) 
 		{
 			Empty(game);
+			drawPlatform(game, platformR, platformC);
+			gravity(game, rhero, chero);
 			drawHero(game, rhero, chero);
 			drawGame(game);
 			Sleep(100);
 		}
+		move = _getch();
+		moveHero(rhero, chero, move, game);
 	}
 }
